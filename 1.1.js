cf416a58fcc6456536828ce85fb0bd1e472bb42a
asafefranca-source <asafe_franca@estudante.sesisenai.org.br>
2026-05-20 22:11:42 -0300

    Add files via upload

---
+++ 1.1.js
@@ -0,0 +1,8 @@
+let prompt = require('prompt-sync')()
+
+let nome
+let idade 
+
+nome = prompt ("digite seu nome ") 
+idade = prompt (" qual é a sua idade ")
+cosole .log (' ola, ${nome}! voce tem ${}idade anos ') 
---
+++ 1.2.js
@@ -0,0 +1,69 @@
+let prompt = require('prompt-sync') ()
+
+
+let nomeProdut =prompt ("nome do produto ?" )
+
+let codProduto =prompt ("qual é o codigo ")
+ 
+let precoProduto =prompt ("qual é o preço do prduto")
+ 
+let categoriaProdut =prompt("qual é a categoria do produto")
+ 
+let estoqueProduto =prompt (" produto esta em estoque ?")
+
+console.log ("nome do produto: " + nomeProdut)
+console.log ("código: " + codProduto )
+console.log ("qual é o preço:  " + precoProduto) 
+console.log ("qual é a categoria do produto: " + categoriaProdut) 
+console.log ("o produto esta em estoque ?: " + estoqueProduto) 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
