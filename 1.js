b017a0bb2af3163f34e8a3de3a9e678f2f0741e3
asafefranca-source <asafe_franca@estudante.sesisenai.org.br>
2026-05-21 22:14:25 -0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,9 @@
+// codigo quebrado:
+console.log(sistema)
+const sistema = "terminal dev"
+{
+  const versao = "2.0.1";
+  let  taxa = 0.15
+   taxa = "0.20";
+console.log (versao)
+}
---
+++ 2.js
@@ -0,0 +1,67 @@
+import promptsync from  'prompt-sync'  
+const prompt = promptsync()
+  
+const anoAtual2026
+let idade
+
+let idade = prompt  (" quantos anos voce tem ?:  ") 
+ 
+anoAtual-idade
+
+
+console.log (`voce nasceu no ano de: ${idade }`) 
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
