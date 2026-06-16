66949f1bac4342bc365a48c80f09bb63a97cbd11
asafefranca-source <asafe_franca@estudante.sesisenai.org.br>
2026-06-15 21:07:50 -0300

    Add files via upload

---
+++ js do zafe/exercios -fuction.js
@@ -0,0 +1,259 @@
+// bloco 1 
+//exercico 1 
+
+function dobrar (n) {
+return n*2
+}
+
+console.log (dobrar(10))
+//----------------------------------
+
+const dobrar = (n)=> {
+  return n* 2 
+}
+//---------------------------------
+
+const dobrar = n => n * 2
+
+//=====================================
+
+//exercicio 2 
+function  somar (a , b ){
+return a + b
+}
+//----------------------------
+const somar  = (a, b)=> {
+  return a + b
+}
+//----------------------------
+
+const somar  = (a, b) =>  a + b 
+
+//=================================
+
+//exercicio 3 
+
+function  somar (a , b ){
+return a - b
+}
+
+const somar  = (a, b)=> {
+  return a - b
+}
+
+
+const somar  = (a, b) =>  a - b 
+
+//========================================
+
+// exercicio 4 
+
+function multiplicarpor10 (n) {
+return n * 10
+}
+
+
+
+const multiplicarpor10 = (n)=> {
+  return n * 10 
+}
+
+
+
+const multiplicar10 = n => n * 10
+
+//=========================================
+
+//exercicio 5
+
+function areaQuadrado (lado) {
+return lado  *lado
+}
+
+
+
+const areaQuadrado = (lado)=> {
+  return lado * lado
+}
+
+
+
+const areaQuadrado = lado => lado* lado 
+//==============================================
+
+//bloco 2
+
+//exercicio 6
+
+function transformarEmMaiuscula  (n) {
+return n.toUpperCase() 
+}
+
+
+const transformarEmMaiuscula  = (n)=> {
+  return n.toUpperCase() 
+  
+}
+
+
+const transformarEmMaiuscula  = n => n.toUpperCase() 
+  
+//======================================================
+
+//exercicio 7
+ 
+let nome 
+
+
+
+function saudar  (nome) {
+return (`ola ${nome}`)
+}
+
+
+ const saudar   = (nome)=> {
+  return (`ola ${nome}`)
+ } 
+
+
+
+ const saudar  = nome => (`ola ${nome}`)
+
+//==============================================
+// exercicio 8 
+
+function tamanhotexto  (obterTamanhoTexto) {
+return (obterTamanhoTexto.length)
+
+}
+
+
+ const tamanhotexto   = (obterTamanhoTexto)=> {
+  return (obterTamanhoTexto .length)
+ } 
+
+
+
+ const tamanhotexto = obterTamanhoTexto => (obterTamanhoTexto.length)
+//====================================================================
+
+// exercicio 9 
+
+function nomecompleto (obterTamanhoTexto) {
+return (obterTamanhoTexto.length)
+
+}
+
+
+ const nomecompleto  = (obterTamanhoTexto)=> {
+  return (obterTamanhoTexto .length)
+ } 
+
+
+
+ const nomecompleto = obterTamanhoTexto => (obterTamanhoTexto.length)
+//==============================================================
+
+// exercicio 10
+
+function primeiraletra  (obterPrimeiraLetra ) {
+return (obterPrimeiraLetra[0] )
+
+}
+
+
+ const primeiraletra   = (obterPrimeiraLetra )=> {
+  return (obterPrimeiraLetra[0])
+ } 
+
+
+
+ const primeiraletra   = obterPrimeiraLetra => (obterPrimeiraLetra[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+                                
